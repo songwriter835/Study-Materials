708c97d94b151b1fb632ef484c6a87ee66f85a6d
writer <songwriter835@gmail.com>
2025-05-21 17:30:00 +0800

    update def

--- common_def.py
+++ common_def.py
@@ -1,11 +1,21 @@
 # -*- coding: utf-8 -*-
 
-def str_to_digital_type(str_):
-    if  isinstance(str_, (int, float)):
-        return str_
-    for cast in (int, float):
-        try:
-            return cast(str_)
-        except ValueError:
-            pass
-    raise TypeError("please enter a number")
+def str_to_digital_type(value: int | float | str) -> int | float:
+
+    """
+    将输入值转换为数字类型（int 或 float）
+    """
+
+    error_text = "please enter a number"
+
+    if isinstance(value, (int, float)):
+        return value
+    if isinstance(value, str):
+        for cast in (int, float):
+            try:
+                return cast(value)
+            except ValueError:
+                continue
+        raise TypeError(error_text)
+    else:
+        raise TypeError(error_text)
